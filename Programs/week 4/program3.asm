2b32a8393d0ff2942a3c0c399f664c75c9e2c1db
RazerBlade6 <124300610+RazerBlade6@users.noreply.github.com>
2024-03-04 15:43:53 +0400

    Add files via upload

--- Programs/week 4/program3.asm
+++ Programs/week 4/program3.asm
@@ -3,28 +3,31 @@
 
 .code
 start:
-    mov al, 45h
-    mov bl, al
-    and al, 0F0h
-    ror al, 4
-    call hexconvert
-    mov dl, al
-    mov ah, 2
-    int 21h
-    mov al, bl
-    and al, 0Fh
-    call hexconvert
-    mov ah, 4Ch
-    int 21
+        mov al, 45h
+        mov bl, al
+        and al, 0F0h
+        ror al, 4
+        call hexconvert
+        mov dl, al
+        mov ah, 2
+        int 21h
+        mov al, bl
+        and al, 0Fh
+        call hexconvert
+        mov dl, al
+        mov ah, 2
+        int 21h
 
-    hexconvert:
-        cmp al, 0Ah
-        jb num
-        add al, 7
-        num:
-        add al, 30h
-    ret   
+        mov ah, 4Ch
+        int 21h
+
+        hexconvert:
+                cmp al, 0Ah
+                jb num
+                add al, 7
+                num:
+                add al, 30h
+        ret
 end start
 
 
-    
