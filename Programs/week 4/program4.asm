1e2173873fd9689631dc8e1eb271fe5c3ded07ab
RazerBlade6 <124300610+RazerBlade6@users.noreply.github.com>
2024-03-04 15:21:34 +0400

    Add files via upload

--- Programs/week 4/program4.asm
+++ Programs/week 4/program4.asm
@@ -3,24 +3,25 @@
 
 .code
 start:
-    call readLine
-    mov bl, al
-    call nextLine
-    call readLine
-    mul bl
-    call nextLine
-    call display
-    mov ah, 4Ch
-    int 21h
+        call readline
+        mov bl, al
+        call nextline
+        call readline
+        mul bl
+        mov bl, al
+        call nextline
+        call disp
+        mov ah, 4Ch
+        int 21h
 
-    readLine proc near
+readline proc near
         mov ah, 01
         int 21h
         call asctohex
         ret
-    readLine endp
+readline endp
 
-    asctohex proc near
+asctohex proc near
         cmp al, 3Ah
         jc sub30
         sub al, 7
@@ -28,40 +29,40 @@ start:
         sub al, 30h
         and al, 0Fh
         ret
-    asctohex endp
-    
-    nextLine proc near
+asctohex endp
+
+nextline proc near
         mov ah, 2
         mov dl, 0Ah
         int 21h
-    nextLine endp
+        mov dl, 0Dh
+        int 21h
+        ret
+nextline endp
 
-    display proc near
+disp proc near
         mov al, bl
         and al, 0F0h
         ror al, 4
         call hexasc
         mov dl, al
-        mov ah, 2
+        mov ah, 02
         int 21h
         mov al, bl
         and al, 0Fh
-        and al, 0Fh
         call hexasc
         mov dl, al
-        int 21
+        mov ah, 2
+        int 21h
         ret
-    display endp
+disp endp
 
-    hexasc proc near
+hexasc proc near
         cmp al, 0Ah
         jb num
         add al, 7
         num:
         add al, 30h
         ret
-    hexasc endp
+hexasc endp
 end start
-
-
-    
